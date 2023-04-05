136fca00a9942e1bbecf31f68f0bfcc3b3639a60
Lucas Rodrigues <rodriguesilva_@outlook.com>
2023-04-05 00:16:53 -0300

    feat: :construction_worker: implementing Jenkins

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,14 @@
 # Imagem base do Go
 FROM golang:latest
 
+# Instalar dependências para o Jenkins
+RUN apt-get update && apt-get install -y wget gnupg
+
+# Adicionar chave GPG do Jenkins e instalar o pacote do Jenkins
+RUN wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | apt-key add -
+RUN sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+RUN apt-get update && apt-get install -y jenkins
+
 # Copiar os arquivos do código fonte
 COPY producer.go /app/
 COPY consumer.go /app/
--- producer.go
+++ producer.go
@@ -1,51 +1,86 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/streadway/amqp"
 )
 
-func sendToRabbitMQ() {
+type Message struct {
+	Message string `json:"message"`
+}
+
+func runProducer() {
+	http.HandleFunc("/send", handleSend)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleSend(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprint(w, "Method not allowed")
+		return
+	}
+
+	var message Message
+	err := json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Invalid request payload")
+		return
+	}
+
+	err = sendMessageToRabbitMQ(message.Message)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Failed to send message to RabbitMQ")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Message sent to RabbitMQ")
+}
+
+func sendMessageToRabbitMQ(message string) error {
 	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+		return err
 	}
 	defer conn.Close()
 
 	ch, err := conn.Channel()
 	if err != nil {
-		log.Fatalf("Failed to open a channel: %v", err)
+		return err
 	}
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		"my-queue", // nome da fila
-		false,      // durabilidade
-		false,      // auto-delete
-		false,      // exclusiva
-		false,      // no-wait
-		nil,        // argumentos extras
+		"hello", // nome da fila
+		false,   // durável
+		false,   // exclusiva
+		false,   // autodelete
+		false,   // sem espera
+		nil,     // argumentos
 	)
 	if err != nil {
-		log.Fatalf("Failed to declare a queue: %v", err)
-	}
-
-	msg := amqp.Publishing{
-		ContentType: "text/plain",
-		Body:        []byte("Hello, RabbitMQ!"),
+		return err
 	}
 
 	err = ch.Publish(
-		"",     // nome da exchange
-		q.Name, // nome da fila
+		"",     // troca
+		q.Name, // rota
 		false,  // mandatório
 		false,  // imediato
-		msg,
-	)
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
 	if err != nil {
-		log.Fatalf("Failed to publish a message: %v", err)
+		return err
 	}
 
-	log.Println("Sent a message to RabbitMQ")
+	return nil
 }
